29b400584205b95347d171f3e2644a8cee332c76
Jon McManus <due@wxwhatever.com>
2020-03-22 23:12:01 +1000

    Improve grindstone tooltips.

--- src/main/java/noobanidus/mods/grindr/items/GrindstoneItem.java
+++ src/main/java/noobanidus/mods/grindr/items/GrindstoneItem.java
@@ -1,10 +1,13 @@
 package noobanidus.mods.grindr.items;
 
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -42,19 +45,30 @@ public class GrindstoneItem extends Item {
       double speed = type.getSpeedModifier() * 100;
       boolean neg = false;
       if (speed > 100) {
-        speed = -(speed - 100);
+        speed = (speed - 100);
         neg = true;
+      } else {
+        speed = (speed - 100);
       }
 
       double multiplier = type.getResultModifier() * 100;
       boolean neg2 = false;
       if (multiplier < 100) {
-        multiplier = -(100 - multiplier);
+        multiplier = (100 - multiplier);
         neg2 = true;
+      } else {
+        multiplier = -(100 - multiplier);
       }
 
-      tooltip.add(new TranslationTextComponent("grinder.tooltip.grindstone.desc1", (neg ? TextFormatting.RED : TextFormatting.GREEN) + ((int) speed + "%")));
-      tooltip.add(new TranslationTextComponent("grinder.tooltip.grindstone.desc2", (neg2 ? TextFormatting.RED : TextFormatting.GREEN) + ((int) multiplier + "%")));
+      tooltip.add(new TranslationTextComponent("grinder.tooltip.grindstone.desc1", (neg ? TextFormatting.RED + "+" : TextFormatting.GREEN) + ((int) speed + "%")));
+      tooltip.add(new TranslationTextComponent("grinder.tooltip.grindstone.desc2", (neg2 ? TextFormatting.RED: TextFormatting.GREEN + "+") + ((int) multiplier + "%")));
+
+      if (Screen.hasShiftDown()) {
+        tooltip.add(new TranslationTextComponent("grinder.tooltip.speed_desc"));
+        tooltip.add(new TranslationTextComponent("grinder.tooltip.result_desc"));
+      } else {
+        tooltip.add(new TranslationTextComponent("grinder.tooltip.shift_for_more").setStyle(new Style().setColor(TextFormatting.GRAY)));
+      }
     }
   }
 }
