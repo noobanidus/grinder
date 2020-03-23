8001776b0faf7a219d3deb3fbc944c098da212b1
Jon McManus <due@wxwhatever.com>
2020-03-23 21:10:40 +1000

    Blurgh.

--- src/main/java/noobanidus/mods/grindr/setup/ClientSetup.java
+++ src/main/java/noobanidus/mods/grindr/setup/ClientSetup.java
@@ -6,10 +6,12 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import noobanidus.mods.grindr.client.screen.GrinderScreen;
 import noobanidus.mods.grindr.init.ModContainers;
+import noobanidus.mods.shoulders.common.bootstrap.Bootstrap;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientSetup {
   public static void init(FMLClientSetupEvent event) {
     ScreenManager.registerFactory(ModContainers.GRINDER_CONTAINER.get(), GrinderScreen::new);
+    Bootstrap.init(event.getMinecraftSupplier().get().getRenderManager().getSkinMap().values());
   }
 }
