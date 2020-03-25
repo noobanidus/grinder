e3e64c355bab13216fa43160eafedfced10bffad
Jon McManus <due@wxwhatever.com>
2020-03-25 11:12:31 +1000

    Fix missing metadata.

--- src/main/java/noobanidus/mods/grindr/setup/ClientSetup.java
+++ src/main/java/noobanidus/mods/grindr/setup/ClientSetup.java
@@ -1,16 +1,12 @@
 package noobanidus.mods.grindr.setup;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import noobanidus.mods.grindr.client.screen.GrinderScreen;
 import noobanidus.mods.grindr.init.ModContainers;
-import noobanidus.mods.shoulders.common.bootstrap.Bootstrap;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientSetup {
@@ -20,10 +16,8 @@ public class ClientSetup {
 
   @SuppressWarnings("deprecation")
   public static void loadComplete(FMLLoadCompleteEvent event) {
-    DistExecutor.runWhenOn(Dist.CLIENT, () -> () ->
-      DeferredWorkQueue.runLater(() ->
+/*    DistExecutor.runWhenOn(Dist.CLIENT, () -> () ->
         Bootstrap.init(Minecraft.getInstance())
-      )
-    );
+    );*/
   }
 }
